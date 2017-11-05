45a9912bc37d86b9666f9f09ef509f8990ce5ab3
Bart Moorman <bmoorman@Xenopus.id10t.us>
2017-11-05 00:56:53 -0600

    Needed? No. Adding to be safe and consistent.

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $OPENVPN_USERNAME > /etc/openvpn/credentials.txt
-echo $OPENVPN_PASSWORD >> /etc/openvpn/credentials.txt
+echo ${OPENVPN_USERNAME} > /etc/openvpn/credentials.txt
+echo ${OPENVPN_PASSWORD} >> /etc/openvpn/credentials.txt
 
 if [ -f /etc/openvpn/openvpn.zip ]; then
     unzip -q -d /etc/openvpn /etc/openvpn/openvpn.zip && rm --force /etc/openvpn/openvpn.zip
@@ -8,7 +8,7 @@ if [ -f /etc/openvpn/openvpn.zip ]; then
 fi
 
 exec $(which openvpn) \
-    --config "/etc/openvpn/$OPENVPN_GATEWAY.ovpn" \
+    --config "/etc/openvpn/${OPENVPN_GATEWAY}.ovpn" \
     --route-up /etc/openvpn/route.sh \
     --ping-exit 60 \
     --ping 10 \
@@ -16,12 +16,12 @@ exec $(which openvpn) \
     --up-delay \
     --down /etc/transmission/stop.sh \
     --down-pre \
-    --setenv OPENVPN_GATEWAY $OPENVPN_GATEWAY \
-    --setenv OPENVPN_LOCAL_NETWORK $OPENVPN_LOCAL_NETWORK \
-    --setenv TRANSMISSION_ALLOWED $TRANSMISSION_ALLOWED \
-    --setenv TRANSMISSION_CACHE $TRANSMISSION_CACHE \
-    --setenv TRANSMISSION_MIN_PORT_HRS $TRANSMISSION_MIN_PORT_HRS \
-    --setenv TRANSMISSION_MAX_PORT_HRS $TRANSMISSION_MAX_PORT_HRS \
+    --setenv OPENVPN_GATEWAY ${OPENVPN_GATEWAY} \
+    --setenv OPENVPN_LOCAL_NETWORK ${OPENVPN_LOCAL_NETWORK} \
+    --setenv TRANSMISSION_ALLOWED ${TRANSMISSION_ALLOWED} \
+    --setenv TRANSMISSION_CACHE ${TRANSMISSION_CACHE} \
+    --setenv TRANSMISSION_MIN_PORT_HRS ${TRANSMISSION_MIN_PORT_HRS} \
+    --setenv TRANSMISSION_MAX_PORT_HRS ${TRANSMISSION_MAX_PORT_HRS} \
     --script-security 2 \
     --cd /etc/openvpn \
     --log /var/log/openvpn.log
--- transmission/start.sh
+++ transmission/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $(which transmission-daemon) \
-    --allowed 127.*.*.*,$TRANSMISSION_ALLOWED \
+    --allowed 127.*.*.*,${TRANSMISSION_ALLOWED} \
     --incomplete-dir /data/incomplete \
     --logfile /var/log/transmisson-daemon.log \
     --config-dir /config \
